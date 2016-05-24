bd0851d99466e2a9ffc59874db5c055791026f76
Johann Reyes <jreyes@vaporwarecorp.com>
2016-05-23 23:07:15 -0400

    Adding grid

--- app/src/main/java/com/vaporwarecorp/mirror/component/datetime/DateTimeView.java
+++ app/src/main/java/com/vaporwarecorp/mirror/component/datetime/DateTimeView.java
@@ -1,7 +1,18 @@
 package com.vaporwarecorp.mirror.component.datetime;
 
-/**
- * Created by Johann on 5/23/2016.
- */
-public class DateTimeView {
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import com.vaporwarecorp.mirror.R;
+
+public class DateTimeView extends LinearLayout {
+// --------------------------- CONSTRUCTORS ---------------------------
+
+    public DateTimeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater.from(context).inflate(R.layout.view_date_time, this, true);
+    }
 }
